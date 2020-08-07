41fea79d07b667d36e42e1eacce0a6f4f7bd2776
fahad <fjkiani@icloud.com>
2020-08-07 15:16:27 -0400

    commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
       <p>
-        Lorem ipsum dolor amet pok pok blue bottle fanny pack bushwick
+        Lorem ipsum dolor amet potetk pok blue bottle fanny pack bushwick
         mumblecore photo booth. Unicorn coloring book letterpress small batch,
         before they sold out pour-over four loko ethical. Gentrify letterpress
         XOXO, kale chips occupy mumblecore pickled cred sustainable. Hot chicken
